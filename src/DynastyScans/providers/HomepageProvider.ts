5aff1e9bc3b0b1a311f75c2f6a497ba0a25ca1c8
Kagari Mimi <105492782+kagari-mimi@users.noreply.github.com>
2025-05-26 01:27:32 +0900

    Cleanup HomepageProvider to allow concurrency

--- src/DynastyScans/providers/HomepageProvider.ts
+++ src/DynastyScans/providers/HomepageProvider.ts
@@ -10,157 +10,136 @@ import * as cheerio from "cheerio/slim";
  * Provides data for discovery from the homepage of the site.
  */
 export class HomepageProvider {
+  // Base URL for the homepage
+  private readonly baseUrl = "https://dynasty-scans.com";
+
   // List of anthology tags to ignore
   private readonly notAnthologies = ["pixiv", "oneshots", "no_title"];
 
-  // Store latest updates from the homepage
-  private latestUpdates: DiscoverSectionItem[] = [];
-
   // Concurrency control for fetching the homepage
-  private isLoading = false;
-  private loadingPromise: Promise<void> | null = null;
+  private isLoadingHomepageHtml = false;
+  private homepageHtmlPromise: Promise<CheerioAPI> | null = null;
 
   /**
    * Returns latest update chapters from the home page.
    */
   async getLatestUpdates(): Promise<PagedResults<DiscoverSectionItem>> {
-    await this.fetchHomepageData();
+    const [$, json] = await Promise.all([
+      this.fetchHomepageHtml(),
+      this.fetchHomepageJson(),
+    ]);
+    const latestUpdates: DiscoverSectionItem[] = [];
 
-    return { items: this.latestUpdates };
+    // Extract latest updates
+    $(".chapters .chapter").each((index, element) => {
+      const chapterData = json.chapters![index];
+      const chapterElement = $(element);
+
+      // Pre-lookup anthology tag and filter out non-anothology
+      const anthologyTag = chapterData.tags.find(
+        (tag) =>
+          tag.type == "Anthology" &&
+          !this.notAnthologies.includes(tag.permalink),
+      );
+
+      // mangaId:
+      // - If this is part of a series, use permalink of the series.
+      // - If this chapter is part of an anthology, use anthology's permalink.
+      // - Otherwise, prefix the chapter's permalink with `oneshot/`.
+      let mangaId = undefined;
+      if (chapterData.series) {
+        mangaId =
+          "series/" +
+          chapterData.tags.find((tag) => tag.type == "Series")!.permalink;
+      } else if (anthologyTag) {
+        mangaId = "anthologies/" + anthologyTag.permalink;
+      } else {
+        mangaId = "oneshots/" + chapterData.permalink;
+      }
+
+      // imageUrl: from `<img>` tag on the HTML page, substitute thumbnail
+      // size to medium size.
+      const imageUrl =
+        "https://dynasty-scans.com" +
+        chapterElement
+          .find("img")
+          .first()
+          .attr("src")
+          ?.replace("thumbnail.jpg", "medium.jpg");
+
+      // chapterId: chapter's permalink (as it's unique).
+      const chapterId = chapterData.permalink;
+
+      // title: Use `series` if available, and fallback to `title` for
+      // oneshots.
+      const title = chapterData.series || chapterData.title;
+
+      // subtitle:
+      // - If the chapter is part of a series, remove the series' title from
+      //   chapter's title to get the chapter number and chapter name.
+      // - If the chapter is part of an anthology, display the anthology's
+      //   name using the tag.
+      // - If the chapter is a oneshot, display the text inside `<small>` tag.
+      //   Usually this will say "Original Doujin"or something.
+      const subtitle = chapterData.series
+        ? chapterData.title
+            .replace(chapterData.series, "")
+            .trim()
+            .replace(/^ch0?/, "Ch. ")
+        : anthologyTag
+          ? anthologyTag.name
+          : chapterElement.find(".title small").first().text() || "Oneshot";
+
+      // contentRating: if it's tagged with NSFW, then it's ADULT.
+      const contentRating = chapterData.tags.some(
+        (tag) => tag.permalink == "nsfw",
+      )
+        ? ContentRating.ADULT
+        : ContentRating.EVERYONE;
+
+      // Construct latest update chapter
+      latestUpdates.push({
+        type: "chapterUpdatesCarouselItem",
+        mangaId: mangaId,
+        chapterId: chapterId,
+        imageUrl: imageUrl,
+        title: title,
+        subtitle: subtitle,
+        contentRating: contentRating,
+      });
+    });
+
+    return { items: latestUpdates };
   }
 
-  // Fetch homepage and extract data for various discovery sections
-  private async fetchHomepageData() {
-    // Blocking other async calls to this functon
-    if (this.isLoading && this.loadingPromise) {
-      await this.loadingPromise;
+  // Fetch HTML content of the homepage
+  private async fetchHomepageHtml(): Promise<CheerioAPI> {
+    // Make other asynchronous calls waiting for the first call
+    if (this.isLoadingHomepageHtml && this.homepageHtmlPromise) {
+      return await this.homepageHtmlPromise;
     } else {
-      this.isLoading = true;
-      this.loadingPromise = (async () => {
-        const requests = [
-          { url: "https://dynasty-scans.com", method: "GET" },
-          { url: "https://dynasty-scans.com/?format=json", method: "GET" },
-        ];
-        let htmlBuffer: ArrayBuffer | null = null;
-        let jsonBuffer: ArrayBuffer | null = null;
-        try {
-          [htmlBuffer, jsonBuffer] = await Promise.all(
-            requests.map((request) =>
-              Application.scheduleRequest(request).then(([, buffer]) => buffer),
-            ),
-          );
-        } catch (error) {
-          console.error("Failed to fetch homepage data:", error);
-          htmlBuffer = jsonBuffer = null;
-        }
-
-        // Parse both HTML and JSON response
-        let $: CheerioAPI | undefined;
-        let chapters: DynastyScans.Chapter[] | undefined;
-        if (htmlBuffer) {
-          const htmlResponseBody =
-            Application.arrayBufferToUTF8String(htmlBuffer);
-          $ = cheerio.load(htmlResponseBody);
-        }
-        if (jsonBuffer) {
-          try {
-            const response = JSON.parse(
-              Application.arrayBufferToUTF8String(jsonBuffer),
-            ) as DynastyScans.PaginatedResponse;
-
-            chapters = response.chapters;
-          } catch (error) {
-            console.error("Failed to parse homepage JSON:", error);
-          }
-        }
-
-        if (chapters && $) {
-          // Extract latest updates
-          $(".chapters .chapter").each((index, element) => {
-            const chapterData = chapters[index];
-            const chapterElement = $(element);
-
-            // Pre-lookup anthology tag and filter out non-anothology
-            const anthologyTag = chapterData.tags.find(
-              (tag) =>
-                tag.type == "Anthology" &&
-                !this.notAnthologies.includes(tag.permalink),
-            );
-
-            // mangaId:
-            // - If this is part of a series, use permalink of the series.
-            // - If this chapter is part of an anthology, use anthology's permalink.
-            // - Otherwise, prefix the chapter's permalink with `oneshot/`.
-            let mangaId = undefined;
-            if (chapterData.series) {
-              mangaId =
-                "series/" +
-                chapterData.tags.find((tag) => tag.type == "Series")!.permalink;
-            } else if (anthologyTag) {
-              mangaId = "anthologies/" + anthologyTag.permalink;
-            } else {
-              mangaId = "oneshots/" + chapterData.permalink;
-            }
-
-            // imageUrl: from `<img>` tag on the HTML page, substitute thumbnail
-            // size to medium size.
-            const imageUrl =
-              "https://dynasty-scans.com" +
-              chapterElement
-                .find("img")
-                .first()
-                .attr("src")
-                ?.replace("thumbnail.jpg", "medium.jpg");
-
-            // chapterId: chapter's permalink (as it's unique).
-            const chapterId = chapterData.permalink;
-
-            // title: Use `series` if available, and fallback to `title` for
-            // oneshots.
-            const title = chapterData.series || chapterData.title;
-
-            // subtitle:
-            // - If the chapter is part of a series, remove the series' title from
-            //   chapter's title to get the chapter number and chapter name.
-            // - If the chapter is part of an anthology, display the anthology's
-            //   name using the tag.
-            // - If the chapter is a oneshot, display the text inside `<small>` tag.
-            //   Usually this will say "Original Doujin"or something.
-            const subtitle = chapterData.series
-              ? chapterData.title
-                  .replace(chapterData.series, "")
-                  .trim()
-                  .replace(/^ch0?/, "Ch. ")
-              : anthologyTag
-                ? anthologyTag.name
-                : chapterElement.find(".title small").first().text() ||
-                  "Oneshot";
-
-            // contentRating: if it's tagged with NSFW, then it's ADULT.
-            const contentRating = chapterData.tags.some(
-              (tag) => tag.permalink == "nsfw",
-            )
-              ? ContentRating.ADULT
-              : ContentRating.EVERYONE;
-
-            // Construct latest update chapters
-            this.latestUpdates.push({
-              type: "chapterUpdatesCarouselItem",
-              mangaId: mangaId,
-              chapterId: chapterId,
-              imageUrl: imageUrl,
-              title: title,
-              subtitle: subtitle,
-              contentRating: contentRating,
-            });
-
-            // Mark the update progress as finished
-            this.isLoading = false;
-            this.loadingPromise = null;
-          });
-        }
+      this.isLoadingHomepageHtml = true;
+      this.homepageHtmlPromise = (async () => {
+        const [, buffer] = await Application.scheduleRequest({
+          url: this.baseUrl,
+          method: "GET",
+        });
+        const html = Application.arrayBufferToUTF8String(buffer);
+        return cheerio.load(html);
       })();
-      await this.loadingPromise;
+
+      return await this.homepageHtmlPromise;
     }
+  }
+
+  // Fetch JSON data of the homepage
+  private async fetchHomepageJson(): Promise<DynastyScans.PaginatedResponse> {
+    const [, buffer] = await Application.scheduleRequest({
+      url: this.baseUrl + "?format=json",
+      method: "GET",
+    });
+    return JSON.parse(
+      Application.arrayBufferToUTF8String(buffer),
+    ) as DynastyScans.PaginatedResponse;
   }
 }
